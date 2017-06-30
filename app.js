1e96450c886a8aa07debb9a4842bddb19ef2659e
teja-vaitla <vaitla.teja@gmail.com>
2017-06-30 18:30:52 +0530

    Listing cities on the index.html

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var express = require("express");
 var app = express();
-app.get('/',function(request,response){
-    response.send('Hi Teja!');
+
+app.use(express.static('public'));
+
+
+app.get('/cities',function(request,response){
+    var cities = ['Lotopia','Caspiana','Indigo'];
+    response.json(cities);
 });
 
 module.exports = app;
--- test.js
+++ test.js
@@ -7,16 +7,41 @@ describe('Requests to the root path', function(){
     it('Returns a 200 status code', function(done){
         request(app)
         .get('/')
-        .expect(200)
-        .end(function(error){
-            if(error){
-                throw error;
-            }
-            done();
-        });
-
+        .expect(200,done);
     });
+    it('Returns a html format', (done) => {
+        request(app)
+            .get('/')
+            .expect('Content-Type',/html/,done);
+    });
+    it('Returns an index file with cities', (done)=>{
+        request(app)
+            .get('/')
+            .expect(/cities/i, done);
+    })
 
+});
+
+
+
+
+//test for cities end point
+describe('Listing cities on /cities', () => {
+    it('Returns 200 status code', (done) => {
+        request(app)
+            .get('/cities')
+            .expect(200,done);
+    });
+    it('Returns a json content', (done) => {
+        request(app)
+            .get('/cities')
+            .expect('Content-Type',/json/,done);
+    });
+    it('Return JSON list of initial cities', (done) => {
+        request(app)
+            .get('/cities')
+            .expect(JSON.stringify(['Lotopia','Caspiana','Indigo']),done);
+    });
 });
 
 
